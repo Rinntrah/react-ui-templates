671526a5806aa3db32f41cbd0058a477a0738a86
Michał Furgał <michal.furgal@sportvision.tech>
2022-05-12 17:11:08 +0200

    x

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import {Button} from "@mui/material";
 function App() {
   return (
     <div className="App">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
 
     </div>
   );
